889f28cc4057d10e1cb1a69bb9da8588cf392872
Nikodin Sedlarevic <49947147+NikiFMF10@users.noreply.github.com>
2022-06-11 15:16:50 +0200

    DOdani ustrezni grafi v vizualizaciji

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,7 +1,14 @@
 # 3. faza: Vizualizacija podatkov
 
 library(ggplot2)
-
+library(ggvis)
+library(dplyr)
+library(rgdal)
+library(mosaic)
+library(maptools)
+library(ggmap)
+library(mapproj)
+library(munsell)
 
 # 1.graf: POTNIŠKI PREVOZ IN PROMET
 
@@ -47,3 +54,67 @@ graf_pogon_in_gorivo <- ggplot(data=indeks, mapping = aes(x=Leto, y=Indeks, grou
   theme(legend.text = element_text(size=8)) + 
   ggtitle("Indeks števila avtomobilov glede na vrsto goriva in pogona") +
   labs(color='Vrsta goriva in pogona')
+
+
+
+# Uvozimo zemljevid.
+
+zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
+                             "SVN_adm1", mapa = 'zemljevid', encoding = "UTF-8")
+
+zemljevid$NAME_1 <- c("Gorenjska", "Goriška","Jugovzhodna", "Koroška", "Primorsko-notranjska", "Obalno-kraška", "Osrednjeslovenska", "Podravska", "Pomurska", "Savinjska", "Posavska", "Zasavska")
+
+zemljevid <- fortify(zemljevid)   
+
+
+
+# 1.ZEMLJEVID: POVPREČNO NETO PLAČA V LETU 2020
+
+zemljevid_neto_placa <- ggplot() + geom_polygon(data=left_join(zemljevid, neto_placa2020, by=c("NAME_1"="Regija")),
+                                                aes(x=long, y=lat, group=group, fill=Neto_placa)) +
+  geom_line() + 
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  guides(fill=guide_colorbar(title="Plača")) +
+  ggtitle("Povprečna neto plača po regijah leta 2020") +
+  labs(x = " ") +
+  labs(y = " ") +
+  scale_fill_gradient(low = "white", high = "red",
+                      space = "Lab", na.value = "#e0e0d1", guide = "black",
+                      aesthetics = "fill")
+
+
+
+# 2.ZEMLJEVID: POVPREČNA STAROST OSEBNEGA AVTOMOBILA GLEDE NA REGIJO
+
+zemljevid_povprecna_starost <- ggplot() + geom_polygon(data=left_join(zemljevid, povprecna_starost2020, by=c("NAME_1"= "Regija")),
+                                                       aes(x=long, y=lat, group=group, fill=Povprecna_starost_avtomobila)) +
+  geom_line() + 
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  guides(fill=guide_colorbar(title="Povprečna starost avtomobila")) +
+  ggtitle("Povprečna starost avtomobila po regijah leta 2020") +
+  labs(x = " ") +
+  labs(y = " ") +
+  scale_fill_gradient(low = "white", high = "red",
+                      space = "Lab", na.value = "#e0e0d1", guide = "black",
+                      aesthetics = "fill")
+
+
+cor(povprecna_starost2020$Povprecna_starost_avtomobila, neto_placa2020$Neto_placa)
+
+
+osebni_avtomobili1 <- filter(osebni_avtomobili, Leto=='2020')
+
+zemljevid_osebni_avtomobili <- ggplot() + geom_polygon(data=left_join(zemljevid, osebni_avtomobili1, by=c("NAME_1"= "Regija")),
+                                                       aes(x=long, y=lat, group=group, fill=Osebni_avtomobili)) +
+  geom_line() + 
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  guides(fill=guide_colorbar(title="Število avtomobilov")) +
+  ggtitle("Število avtomobilov na 1000 prebivalcev leta 2020") +
+  labs(x = " ") +
+  labs(y = " ") +
+  scale_fill_gradient(low = "white", high = "red",
+                      space = "Lab", na.value = "#e0e0d1", guide = "black",
+                      aesthetics = "fill")

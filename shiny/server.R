5384c1b00dd2d5d8e1658d08ad7bba01669f66fd
Nikodin Sedlarevic <49947147+NikiFMF10@users.noreply.github.com>
2022-06-15 21:24:19 +0200

    Urejanje povezave preko shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,2 +1,11 @@
 library(shiny)
 
+function(input, output) {
+  output$regija1 <- renderPlot({
+    graf.regije(input$regija)
+  })
+  output$avto <- renderPlot({
+    graf_osebni_avto(input$Leto) 
+  })
+  
+}
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,1 +1,24 @@
 library(shiny)
+
+shinyUI(fluidPage(
+  
+  titlePanel(""),
+  
+  tabsetPanel(
+    tabPanel("Število umrlih v cestnoprometnih nesrečah na 10.000 registriranih motornih vozil",
+             sidebarPanel(
+               checkboxGroupInput(inputId="regija", label = "Izberi regije:",
+                                  choiceNames=c("Gorenjska", "Goriška", "Jugovzhodna", "Koroška", "Obalno-kraška", "Osrednjeslovenska", "Podravska", "Pomurska", "Posavska", "Primorsko-notranjska", "Savinjska", "Zasavska"),
+                                  choiceValues =c("Gorenjska", "Goriška", "Jugovzhodna", "Koroška", "Obalno-kraška", "Osrednjeslovenska", "Podravska", "Pomurska", "Posavska", "Primorsko-notranjska", "Savinjska", "Zasavska"),
+                                  selected = "Jugovzhodna")
+             ),
+             mainPanel(plotOutput("regija1"))),
+    
+    tabPanel("Število osebnih avtomobilov na 1000 prebivalcev",
+             sidebarPanel(
+               sliderInput("Leto", "Izberi leto", min = 2004, max = 2020,
+                           value = 2004, step = 1, sep='', animate = animationOptions(interval=250))
+             ),
+             mainPanel(plotOutput("avto")))
+  )
+))
